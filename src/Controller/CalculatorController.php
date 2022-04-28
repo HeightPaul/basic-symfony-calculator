ad18adc7efa7b2dcbf7f84369aaba476d8197196
HeightPaul <hristopaul@gmail.com>
2022-04-28 17:40:46 +0300

    Code sniffer setup

--- src/Controller/CalculatorController.php
+++ src/Controller/CalculatorController.php
@@ -5,13 +5,17 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
+/**
+ * Controller for calculator actions
+ */
 class CalculatorController extends AbstractController
 {
     /**
      * @Route("/calculator", name="calculator_index")
      */
-    public function index()
+    public function index(): void
     {
-        var_dump('bravo');exit;
+        var_dump('bravo');
+        exit;
     }
 }
